9279867b289b72067e0847aebf5d92be9417eb05
Khetesh Singh Rajpurohit <55997201+KheteshS@users.noreply.github.com>
2026-08-21 16:46:33 +0530

    Time: 13 ms (20.12%), Space: 46.7 MB (82.79%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,40 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // brute force
         int n = nums.length;
-        
         if(n == 1) {
             return nums[0];
         }
-        for( int i = 0; i<n;i++) {
-            int num = nums[i];
-            int count = 0;
-            for(int j = 0; j< n ;j++) {
-                if(nums[j] == num) {
-                    count++;
-                }
-            }
 
-            if(count == 1) {
+        // ----------------------brute force--------------------------
+        // for( int i = 0; i<n;i++) {
+        //     int num = nums[i];
+        //     int count = 0;
+        //     for(int j = 0; j< n ;j++) {
+        //         if(nums[j] == num) {
+        //             count++;
+        //         }
+        //     }
+
+        //     if(count == 1) {
+        //         return num;
+        //     }
+        // }
+        // return -1;
+
+        // ------------------------better approach-------------------
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for( int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+
+        for(int num: nums) {
+            if(freq.get(num) == 1) {
                 return num;
             }
         }
 
         return -1;
+        
     }
 }
