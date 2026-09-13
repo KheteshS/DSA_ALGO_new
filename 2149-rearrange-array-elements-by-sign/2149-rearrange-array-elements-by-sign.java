5d83c798d609c89059158227ee4258870fdeac08
Khetesh Singh Rajpurohit <55997201+KheteshS@users.noreply.github.com>
2026-09-13 22:51:34 +0530

    Time: 9 ms (11.23%), Space: 174.5 MB (83.72%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -25,19 +25,55 @@ class Solution {
         // Time Complexity: O(N) { O(N) for traversing the array once and substituting positives and negatives simultaneously using pointers, where N = size of the array A}.
         // Space Complexity: O(N) { Extra Space used to store the rearranged elements separately in an array, where N = size of array A}.
 
-        int posIndex = 0, negIndex = 1;
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < 0) {
-                ans[negIndex] = nums[i];
-                negIndex += 2;
+        // int posIndex = 0, negIndex = 1;
+        // int[] ans = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] < 0) {
+        //         ans[negIndex] = nums[i];
+        //         negIndex += 2;
 
-            } else {
-                ans[posIndex] = nums[i];
-                posIndex += 2;
+        //     } else {
+        //         ans[posIndex] = nums[i];
+        //         posIndex += 2;
+        //     }
+        // }
+
+        // return ans;
+
+        // ----------------------------whne unequal array is there------------------------------
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                pos.add(nums[i]);
+            else
+                neg.add(nums[i]);
+        }
+
+        if (pos.size() > neg.size()) {
+            for (int i = 0; i < neg.size(); i++) {
+                nums[2 * i] = pos.get(i);
+                nums[2 * i + 1] = neg.get(i);
+            }
+            int index = neg.size() * 2;
+            for (int i = neg.size(); i < pos.size(); i++) {
+                nums[index] = pos.get(i);
+                index++;
+            }
+
+        } else {
+            for (int i = 0; i < pos.size(); i++) {
+                nums[2 * i] = pos.get(i);
+                nums[2 * i + 1] = neg.get(i);
+            }
+            int index = pos.size() * 2;
+            for (int i = pos.size(); i < neg.size(); i++) {
+                nums[index] = neg.get(i);
+                index++;
             }
         }
 
-        return ans;
+        return nums;
     }
 }
