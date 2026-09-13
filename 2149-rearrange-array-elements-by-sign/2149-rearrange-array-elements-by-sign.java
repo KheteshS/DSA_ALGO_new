6074eb3f3e0a95bcadff0bec73618aed54cfab29
Khetesh Singh Rajpurohit <55997201+KheteshS@users.noreply.github.com>
2026-09-13 22:35:04 +0530

    Time: 3 ms (100%), Space: 175 MB (78.84%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,20 +1,40 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
+        // ------------------Brute-----------------------
+        // Time Complexity: O(N+N/2) { O(N) for traversing the array once for segregating positives and negatives and another O(N/2) for adding those elements alternatively to the array, where N = size of the array A}.
+        // Space Complexity: O(N/2 + N/2) = O(N) { N/2 space required for each of the positive and negative element arrays, where N = size of the array A}.
 
+        // List<Integer> pos = new ArrayList<>();
+        // List<Integer> neg = new ArrayList<>();
+
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > 0)
+        //         pos.add(nums[i]);
+        //     else
+        //         neg.add(nums[i]);
+        // }
+
+        // for (int i = 0; i < n / 2; i++) {
+        //     nums[2 * i] = pos.get(i);
+        //     nums[2 * i + 1] = neg.get(i);
+        // }
+        // return nums;
+
+        // ---------------------better-----------------------------
+        int posIndex = 0, negIndex = 1;
+        int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0)
-                pos.add(nums[i]);
-            else
-                neg.add(nums[i]);
+            if (nums[i] < 0) {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+
+            } else {
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
         }
 
-        for (int i = 0; i < n / 2; i++) {
-            nums[2 * i] = pos.get(i);
-            nums[2 * i + 1] = neg.get(i);
-        }
-        return nums;
+        return ans;
     }
 }
